34fa5678f481f5d13c2b60ad08a29c8ea8f6008e
FarisHollow <mirfaris79@gmail.com>
2023-06-27 23:53:36 +0600

    To Do List App

--- lib/todo.dart
+++ lib/todo.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _titleTEController = TextEditingController();
   final TextEditingController _descriptionTEController = TextEditingController();
@@ -42,6 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               const SizedBox(height: 7),
               TextFormField(
+                maxLines: 6,
                 controller: descriptionController,
                 decoration: const InputDecoration(
                   labelText: "Description",
@@ -96,6 +98,43 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
 
+  void _showTodoDetails(int index) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Task Details',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Title: ${todos[index].title}',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                'Description: ${todos[index].description}',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Close'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
 
 
@@ -153,53 +192,58 @@ class _HomeScreenState extends State<HomeScreen> {
           // if (true) {
           //   const Text('Hello');
           // }
-          return ListTile(
-            onLongPress: () {
-              todos[index].isDone = !todos[index].isDone;
-              if (mounted) {
-                setState(() {});
-              }
-            },
-            leading: Visibility(
-              visible: todos[index].isDone,
-              replacement: const Icon(Icons.close),
-              child: const Icon(Icons.done_outline),
+          return Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListTile(
+              onLongPress: () {
+
+                todos[index].isDone = !todos[index].isDone;
+                if (mounted) {
+                  setState(() {});
+                }
+              },
+              onTap:() {_showTodoDetails(index);},
+              leading: Visibility(
+                visible: todos[index].isDone,
+                replacement: const Icon(Icons.close),
+                child: const Icon(Icons.done_outline),
+              ),
+              title: Text(todos[index].title),
+              subtitle: Text(
+                todos[index].description,
+              ),
+
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+
+                      _editTodo(index);
+
+                    },
+                  ),
+
+                  IconButton(
+                    icon: const Icon(Icons.delete_forever_outlined),
+                    onPressed: () {
+                      _deleteTodoConfirmation(index);
+
+
+                      // todos.removeAt(index);
+                      // if (mounted) {
+                      //   setState(() {});
+                      // }
+
+
+                    },
+                  ),
+                ],
+              ),
+
             ),
-            title: Text(todos[index].title),
-            subtitle: Text(
-              todos[index].description,
-            ),
-
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () {
-
-                    _editTodo(index);
-
-                  },
-                ),
-
-                IconButton(
-                  icon: const Icon(Icons.delete_forever_outlined),
-                  onPressed: () {
-                    _deleteTodoConfirmation(index);
-
-
-                    // todos.removeAt(index);
-                    // if (mounted) {
-                    //   setState(() {});
-                    // }
-
-
-                  },
-                ),
-              ],
-            ),
-
           );
         },
         separatorBuilder: (context, index) {
@@ -234,77 +278,80 @@ class _HomeScreenState extends State<HomeScreen> {
         builder: (context) {
           return Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: todoForm,
-              child: Column(
-                children: [
-                  const Text('Add New List', style: TextStyle(
-                    fontSize: 18,
-                  ),),
-                  SizedBox(height: 10,),
-                  TextFormField(
-                    controller: _titleTEController,
-                    decoration: InputDecoration(
-                        labelText: "Enter Title",
-                        hintText: "Title",
+            child: SingleChildScrollView(
+              child: Form(
+                key: todoForm,
+                child: Column(
+                  children: [
+                    const Text('Add New List', style: TextStyle(
+                      fontSize: 18,
+                    ),),
+                    SizedBox(height: 10,),
+                    TextFormField(
+                      controller: _titleTEController,
+                      decoration: InputDecoration(
+                          labelText: "Enter Title",
+                          hintText: "Title",
 
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.amber,
-                              width: 20,
-                            )
-                        )
-                    ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.amber,
+                                width: 20,
+                              )
+                          )
+                      ),
 
 
-                    validator: (String? value) {
-                      if (value?.trim().isEmpty ?? true) {
-                        return 'Please enter your title';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 7 ,),
-
-                  TextFormField(
-                    controller: _descriptionTEController,
-                    decoration: InputDecoration(
-                        labelText: "Enter Description",
-                        hintText: "Description",
-
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.amber,
-                              width: 20,
-                            )
-                        )
-                    ),
-                    validator: (String? value) {
-                      if (value?.trim().isEmpty ?? true) {
-                        return 'Enter your description';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20 ,),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (todoForm.currentState!.validate()) {
-                        todos.add(Todo(_titleTEController.text.trim(),
-                            _descriptionTEController.text.trim(), false));
-                        if (mounted) {
-                          setState(() {});
+                      validator: (String? value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return 'Please enter your title';
                         }
-                        _titleTEController.clear();
-                        _descriptionTEController.clear();
-                        Navigator.pop(context);
-                      }
-                    },
-                    child: const Text('Add'),
-                  )
-                ],
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 7 ,),
+
+                    TextFormField(
+                      maxLines: 6,
+                      controller: _descriptionTEController,
+                      decoration: InputDecoration(
+                          labelText: "Enter Description",
+                          hintText: "Description",
+
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                color: Colors.amber,
+                                width: 20,
+                              )
+                          )
+                      ),
+                      validator: (String? value) {
+                        if (value?.trim().isEmpty ?? true) {
+                          return 'Enter your description';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20 ,),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (todoForm.currentState!.validate()) {
+                          todos.add(Todo(_titleTEController.text.trim(),
+                              _descriptionTEController.text.trim(), false));
+                          if (mounted) {
+                            setState(() {});
+                          }
+                          _titleTEController.clear();
+                          _descriptionTEController.clear();
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: const Text('Add'),
+                    )
+                  ],
+                ),
               ),
             ),
           );
